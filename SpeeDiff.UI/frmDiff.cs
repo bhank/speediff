fd861177c2ec4acedf13c523fea05e8997f299dc
Adam Coyne <github@mail2.coyne.nu>
2015-03-20 22:20:16 -0500

    Show colored diff

--- SpeeDiff.UI/frmDiff.cs
+++ SpeeDiff.UI/frmDiff.cs
@@ -4,9 +4,13 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DiffPlex;
+using DiffPlex.DiffBuilder;
+using DiffPlex.DiffBuilder.Model;
 
 namespace CoyneSolutions.SpeeDiff
 {
@@ -18,6 +22,7 @@ namespace CoyneSolutions.SpeeDiff
         {
             InitializeComponent();
 
+            rtbLeft.ReadOnly = rtbRight.ReadOnly = true;
             rtbLeft.vScroll += (msg) =>
             {
                 msg.HWnd = rtbRight.Handle;
@@ -38,7 +43,7 @@ namespace CoyneSolutions.SpeeDiff
                     new ColumnHeader {Text = "Author", Width = 50},
                     new ColumnHeader {Text = "Message", Width = 750},
                 }
-            );
+                );
             //lvwRevisions.Items.Add(new ListViewItem(new[] {"1", "2", "3", "4"}));
             //lvwRevisions.Items.Add(new ListViewItem(new[] {"1", "2", "3", "4"}));
             //lvwRevisions.Items.Add(new ListViewItem(new[] {"1", "2", "3", "4"}));
@@ -49,7 +54,7 @@ namespace CoyneSolutions.SpeeDiff
             //Test1();
         }
 
-        async void frmDiff_Load(object sender, EventArgs e)
+        private async void frmDiff_Load(object sender, EventArgs e)
         {
             svnRevisionProvider = new SvnRevisionProvider();
             svnRevisionProvider.RevisionLoaded += (s, args) =>
@@ -62,7 +67,7 @@ namespace CoyneSolutions.SpeeDiff
             await svnRevisionProvider.Initialize(Environment.GetCommandLineArgs()[1]);
         }
 
-        void lvwRevisions_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        private void lvwRevisions_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
         {
             if (e.IsSelected)
             {
@@ -71,16 +76,73 @@ namespace CoyneSolutions.SpeeDiff
                 {
                     index--; // -1 now if it's the only item.
                 }
+                string right, left;
                 if (index == -1)
                 {
-                    rtbLeft.Text = rtbRight.Text = svnRevisionProvider.Revisions[0].GetContent();
+                    left = right = svnRevisionProvider.Revisions[0].GetContent();
                 }
                 else
                 {
-                    rtbRight.Text = svnRevisionProvider.Revisions[index].GetContent();
-                    rtbLeft.Text = svnRevisionProvider.Revisions[index + 1].GetContent();
+                    right = svnRevisionProvider.Revisions[index].GetContent();
+                    left = svnRevisionProvider.Revisions[index + 1].GetContent();
+                }
+                var builder = new SideBySideDiffBuilder(new Differ());
+                var model = builder.BuildDiffModel(left, right);
+
+                ModelToTextBox(model.OldText, rtbLeft);
+                ModelToTextBox(model.NewText, rtbRight);
+
+            }
+        }
+
+        private static void ModelToTextBox(DiffPaneModel model, RichTextBox textBox)
+        {
+            foreach (var line in model.Lines)
+            {
+                if (line.SubPieces.Count == 0)
+                {
+                    AppendText(textBox, line.Text + Environment.NewLine, GetPieceColor(line.Type));
+                }
+                else
+                {
+                    foreach (var piece in line.SubPieces)
+                    {
+                        if (piece.Text != null)
+                        {
+                            AppendText(textBox, piece.Text, GetPieceColor(piece.Type));
+                        }
+                    }
+                    textBox.AppendText(Environment.NewLine);
                 }
             }
+        }
+
+        private static Color GetPieceColor(ChangeType changeType)
+        {
+            switch (changeType)
+            {
+                case ChangeType.Deleted:
+                    return Color.Red;
+                case ChangeType.Imaginary:
+                    return Color.Blue;
+                case ChangeType.Inserted:
+                    return Color.Green;
+                case ChangeType.Modified:
+                    return Color.Orange;
+                case ChangeType.Unchanged:
+                    return Color.Black;
+            }
+            return Color.Aqua; // ?
+        }
+
+        private static void AppendText(RichTextBox box, string text, Color color)
+        {
+            box.SelectionStart = box.TextLength;
+            box.SelectionLength = 0;
+
+            box.SelectionColor = color;
+            box.AppendText(text);
+            box.SelectionColor = box.ForeColor;
         }
     }
 }
