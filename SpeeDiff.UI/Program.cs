3b0105c5ba3afe5994f3c2a476e71f37844819b9
Adam Coyne <github@mail2.coyne.nu>
2015-03-21 23:11:01 -0500

    Remove old test code

--- SpeeDiff.UI/Program.cs
+++ SpeeDiff.UI/Program.cs
@@ -1,19 +1,16 @@
 ﻿// Test with bad path
 // Test with UTF8 (need to pass it to streamreader ctor?)
-// Separate line numbers into different (scroll-synced) textboxes, so you can copy and paste
+// DONE // Separate line numbers into different (scroll-synced) textboxes, so you can copy and paste
 // DONE // Maintain scroll position when switching revisions
 // Save sizes of columns in listview, and window size
 // Maybe make listview sortable...
-// Enable flipping through changes with Ctrl-Up and Ctrl-Down
+// DONE // Enable flipping through changes with Ctrl-Up and Ctrl-Down
 // Use a faster text box!
 // Maybe use a cool text box with syntax highlighting
+// Add a loading spinner
+// Add a keyboard shortcut to jump to the text box
+// Enable viewing in an external diff viewer of your choice
 using System;
-using System.CodeDom;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using CoyneSolutions.SpeeDiff;
 
@@ -27,88 +24,9 @@ namespace speediff
         [STAThread]
         static void Main()
         {
-            //Console.WriteLine("Hey!");
-            //Test1();
-            //Console.ReadKey();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmDiff());
-        }
-
-        private static void Test1()
-        {
-            var revisionProvider = RevisionProvider.GetRevisionProvider(Environment.GetCommandLineArgs()[1]);
-            Task.WaitAll(revisionProvider.Initialize());
-
-            var currentRevisionIndex = revisionProvider.Revisions.Count - 1;
-            while(true)
-            {
-                CompareWithPreviousRevision(revisionProvider.Revisions, currentRevisionIndex);
-                while (true)
-                {
-                    var key = Console.ReadKey(true);
-                    if (key.Key == ConsoleKey.LeftArrow)
-                    {
-                        if (currentRevisionIndex > 0)
-                        {
-                            currentRevisionIndex--;
-                            break;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Already on first revision!");
-                        }
-                    }
-                    if (key.Key == ConsoleKey.RightArrow)
-                    {
-                        if (currentRevisionIndex < revisionProvider.Revisions.Count - 1)
-                        {
-                            currentRevisionIndex++;
-                            break;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Already on last revision!");
-                        }
-                    }
-                    if (key.Key == ConsoleKey.Q) Environment.Exit(0);
-                    Console.Write("?");
-                }
-
-            }
-        }
-
-        private static void CompareWithPreviousRevision(IList<Revision> revisions, int currentRevisionIndex)
-        {
-            if (currentRevisionIndex == 0 || currentRevisionIndex >= revisions.Count) return;
-
-            var leftFile = WriteTempFile(revisions[currentRevisionIndex - 1].GetContent());
-            var rightFile = WriteTempFile(revisions[currentRevisionIndex].GetContent());
-            RunDiff(leftFile, rightFile);
-            File.Delete(leftFile);
-            File.Delete(rightFile);
-        }
-
-        private static void RunDiff(string leftFile, string rightFile)
-        {
-            var process = new Process
-            {
-                StartInfo =
-                    {
-                        FileName = "tortoisemerge.exe",
-                        Arguments = string.Format(@"""{0}"" ""{1}""", leftFile, rightFile),
-                        CreateNoWindow = true
-                    }
-            };
-            process.Start();
-            process.WaitForExit();
-        }
-
-        private static string WriteTempFile(string content)
-        {
-            var fileName = Path.GetTempFileName();
-            File.WriteAllText(fileName, content);
-            return fileName;
         }
     }
 }
