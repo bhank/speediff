ca608fe684b035b79cb4b5d4eb366badbdb2c626
Adam Coyne <github@mail2.coyne.nu>
2015-03-29 18:17:11 -0500

    Update notes

--- speediff/Program.cs
+++ speediff/Program.cs
@@ -1,5 +1,5 @@
-﻿// Test with bad path
-// Test with UTF8 (need to pass it to streamreader ctor?)
+﻿// DONE // Test with bad path
+// DONE // Test with UTF8 (need to pass it to streamreader ctor?)
 // DONE // Separate line numbers into different (scroll-synced) textboxes, so you can copy and paste
 // DONE // Maintain scroll position when switching revisions
 // DONE // Save sizes of columns in listview, and window size
@@ -9,7 +9,7 @@
 // Maybe use a cool text box with syntax highlighting
 // Add a loading spinner
 // DONE // Add a keyboard shortcut to jump to the file text box
-// Enable viewing in an external diff viewer of your choice (maybe right-click a revision)
+// DONE // Enable viewing in an external diff viewer of your choice (maybe right-click a revision)
 // Make listview scroll when you Alt-Left or Alt-Right offscreen
 // DONE // Maybe change the textbox to a dropdown with recent values
 // DONE // Add real icons
@@ -24,7 +24,7 @@
 // Limit revisions loaded, for speed?
 // DONE // Fix horizontal scroll sync to refresh properly -- and don't sync it to the line number textboxes
 // Maybe add a current-line-compare box at the bottom?
-// Right-click to view entire log (do I get that info already?), or to view it in TortoiseSVN
+// DONE // Right-click to view entire log (do I get that info already?), or to view it in TortoiseSVN
 // Maybe jump to the first change, if you're on the first line of the file?
 // DONE // Maybe show a count of the changes, like p4merge? Types too. (could go in a status bar)
 // DONE // Set focus to left or right textbox after selecting a revision, so you can ctrl-home to the top and such
@@ -37,6 +37,14 @@
 // Test with bad server certificate
 // Add blame-ish "go back to where this line changed" function?
 // Show blame info somehow, if I have it already?
+// DONE // Test whether I need to calculate an offset factor for huge horizontal scrolling, like I do for vertical -- no, it forcibly wraps them, but I need to make the line numbers handle that
+// DONE // Does hitting Enter on Browse when the dropdown is open not work?
+// DONE // Line number problem? Seen with SVN unicode resource file
+// Trap SVN-server-unreachable error
+// Enable dragging files into the window to load them
+// BugzID link
+// DONE // Widen dropdown
+// Show only the first line of the description in the listview, or else put a space between lines
 
 using System;
 using System.Windows.Forms;
